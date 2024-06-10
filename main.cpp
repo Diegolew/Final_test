b1d264141380ca8b1f5616d909ada383232e159b
Lewis <diegolewensztain@gmailensztain@gmail.com>
2024-06-09 23:47:22 -0400

    Declaracion de las funciones

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+void Add(string date, string event);
+void DelEvent(string date, string event);
+void DelDate(string date);
+void Find(string date);
+void Print();
