e0d1a06cb4b110c4a355c2d9b0440b9e4c027436
wydiego <diegolewenszta>
2024-06-10 00:01:12 -0400

    Incio del main, para almacenar datos

--- main.cpp
+++ main.cpp
@@ -11,3 +11,41 @@ void DelEvent(string date, string event);
 void DelDate(string date);
 void Find(string date);
 void Print();
+
+map<string, vector<string>> db;
+
+int main(){
+
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        string command;
+        cin >> command;
+        if(command == "Add"){
+            string date, event;
+            cin >> date >> event;
+            Add(date, event);
+        }
+        else if(command == "DelEvent"){
+            string date, event;
+            cin >> date >> event;
+            DelEvent(date, event);
+        }
+        else if(command == "DelDate"){
+            string date;
+            cin >> date;
+            DelDate(date);
+        }
+        else if(command == "Find"){
+            string date;
+            cin >> date;
+            Find(date);
+        }
+        else if(command == "Print"){
+            Print();
+        }
+    }
+
+
+    return 0;
+}
